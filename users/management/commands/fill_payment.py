0e84f8545c184986aec20663649edffbf3fcb9f6
Alexandr Zubarev <zualvi@yandex.ru>
2024-01-24 21:09:31 +0300

    HW_24.2 addition

--- users/management/commands/fill_payment.py
+++ users/management/commands/fill_payment.py
@@ -14,5 +14,10 @@ class Command(BaseCommand):
                                               lesson=Lesson.objects.get(pk=2),
                                               payment_amount=5000,
                                               payment_method='cash'))
+        for user in User.objects.all():
+            payment_for_create.append(Payment(user=user,
+                                              course=Course.objects.get(pk=2),
+                                              payment_amount=10000,
+                                              payment_method='transfer'))
 
         Payment.objects.bulk_create(payment_for_create)
